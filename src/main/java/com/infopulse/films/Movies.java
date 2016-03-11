3437b90dfa85acf2b0ff0a57ad12e03a393697c0
Dmitriy N <dmitriy.smm@gmail.com>
2016-03-12 00:13:14 +0200

    1/Added initial page. 2/Appl get all files from directory /content

--- src/main/java/com/infopulse/films/Movies.java
+++
@@ -1,9 +0,0 @@
-package com.infopulse.films;
-
-public class Movies {
-    public static final String THREE_HUNDRED = "300.txt";
-    public static final String GREEN_BEAUTIFUL = "the_green_beautiful.txt";
-    public static final String UGLY_TRUTH = "the_ugly_truth.txt";
-    public static final String INCORRECT = "incorrect.txt";
-
-}
--- src/main/java/com/infopulse/server/AdvancedHttpServer.java
+++ src/main/java/com/infopulse/server/AdvancedHttpServer.java
@@ -1,52 +1,43 @@
 package com.infopulse.server;
 
-import com.infopulse.films.Movies;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class AdvancedHttpServer {
+    final String DIRECTORY = "./content";
 
     public static void main(String[] args) throws IOException {
         new AdvancedHttpServer().run();
     }
 
     private void run() throws IOException {
+        List<String> listFiles = getListOfFiles(DIRECTORY);
         ServerSocket serverSocket = new ServerSocket(3000);
+        System.out.println("Server started: " + (new Date()));
 
         while (true) {
             try (Socket socket = serverSocket.accept();
                  BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                  BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
 
-                // header bellow
-                bufferedWriter.write("HTTP/1.0 200 OK\r\n");
-                bufferedWriter.write("Server: Apache/0.8.4\r\n");
-                bufferedWriter.write("Content-Type: text/html\r\n");
-                bufferedWriter.write("\r\n");
-                // content bellow
-                bufferedWriter.write("<TITLE>Movie description</TITLE>");
-
-                String fileConsistents = null;
                 String urlPattern = getHeaderFromRequest(bufferedReader);
+
                 if (!"favicon.ico".equals(urlPattern)) {
-                    switch (urlPattern) {
-                        case "300":
-                            fileConsistents = getInfoFromFile(Movies.THREE_HUNDRED);
-                            break;
-                        case "green":
-                            fileConsistents = getInfoFromFile(Movies.GREEN_BEAUTIFUL);
-                            break;
-                        case "truth":
-                            fileConsistents = getInfoFromFile(Movies.UGLY_TRUTH);
-                            break;
-                        default:
-                            fileConsistents = getInfoFromFile(Movies.INCORRECT);
-                            break;
+                    if ("".equals(urlPattern)) {
+                        createInitialPage(bufferedWriter, listFiles);
+                    } else {
+                        for (String file : listFiles) {
+                            if (file.equals(urlPattern)) {
+                                createFilePage(bufferedWriter, file);
+                                break;
+                            }
+                        }
                     }
                 }
-                bufferedWriter.write("<P>" + fileConsistents + "</P>");
             }
         }
     }
@@ -76,5 +67,78 @@ public class AdvancedHttpServer {
             e.getMessage();
         }
         return urlPattern;
+    }
+
+    // Method for creating initial page
+    private void createInitialPage(BufferedWriter bufferedWriter, List<String> fileList) {
+        try {
+            //List<String> fileList = getListOfFiles(DIRECTORY);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            createHeader(stringBuilder);
+
+            stringBuilder.append("<h2>List of files:</h2>\n");
+            stringBuilder.append("<ul>");
+
+            for (String fileName : fileList) {
+                stringBuilder.append("<li><a href=\"http://localhost:3000/" + fileName + "\">" + fileName + "</a></li>\n");
+            }
+            stringBuilder.append("</ul>");
+            createFooterInitialPage(stringBuilder);
+
+            bufferedWriter.write(stringBuilder.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createFilePage(BufferedWriter bufferedWriter, String fileName){
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            createHeader(stringBuilder);
+            stringBuilder.append(getInfoFromFile(fileName));
+            createFooterFilePage(stringBuilder);
+            bufferedWriter.write(stringBuilder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createHeader(StringBuilder stringBuilder) throws IOException {
+        stringBuilder.append("HTTP/1.0 200 OK\r\n");
+        stringBuilder.append("Content-Type: text/html\r\n");
+        stringBuilder.append("\r\n");
+        stringBuilder.append("<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>HTTPServer_v.1.0</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<br>\n");
+    }
+
+    private void createFooterInitialPage(StringBuilder stringBuilder) throws IOException {
+        stringBuilder.append("</body></html>");
+    }
+
+    private void createFooterFilePage(StringBuilder stringBuilder) throws IOException {
+        stringBuilder.append("\n<br><br><a href=\"http://localhost:3000/\"</a>Back to main page</body></html>");
+    }
+
+    // get list of files from folder "resources/content/"
+    private List<String> getListOfFiles(String directory) throws IOException {
+        List<String> fileList = new ArrayList<>();
+        ClassLoader loader = AdvancedHttpServer.class.getClassLoader();
+
+        try (InputStream inputStream = loader.getResourceAsStream(directory);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fileList.add(line);
+            }
+        }
+        return fileList;
+
     }
 }
